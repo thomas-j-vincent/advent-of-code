2ce067742bcfde11b2e729a18fceb4de382904a4
thomas-j-vincent <thomasjvincent@outlook.com>
2025-12-31 20:29:44 +0000

    removed uneccessary files

--- Day5/day 5 test file.py
+++
@@ -1,6 +0,0 @@
-with open("AdventOfCodeDay5Range.txt") as f:
-    between = [line.strip() for line in f]
-
-for r in between:
-    start, end = map(int, r.split("-"))
-    print(r, "size:", end - start + 1)
